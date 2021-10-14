0e6eac4a14cb61398ebf7815eed0cb3b512196a4
Ben Hynes <34022486+benhynes@users.noreply.github.com>
2021-10-14 12:49:14 -0700

    Adding user field in completions (#3)
    * Added user parameter for completions
    
    * Documentation for user parameter

--- Sources/OpenAI/Client.swift
+++ Sources/OpenAI/Client.swift
@@ -119,6 +119,7 @@ public final class Client {
        - echo: Echo back the prompt in addition to the completion.
        - stop: Up to 4 sequences where the API will stop generating further tokens.
                The returned text will not contain the stop sequence.
+       - user: A unique user identifier sometimes required by OpenAI for production.
        - presencePenalty: Number between 0 and 1 that penalizes new tokens
                           based on whether they appear in the text so far.
                           Increases the model's likelihood to talk about new topics.
@@ -148,6 +149,7 @@ public final class Client {
                             numberOfCompletions: Int? = nil,
                             echo: Bool? = nil,
                             stop: [String]? = nil,
+                            user: String? = nil,
                             presencePenalty: Double? = nil,
                             frequencyPenalty: Double? = nil,
                             bestOf: Int? = nil,
@@ -156,6 +158,7 @@ public final class Client {
         var parameters: [String: Any?] = [
             "prompt": prompt,
             "max_tokens": numberOfTokens?.upperBound,
+            "user": user,
             "n": numberOfCompletions,
             "echo": echo,
             "stop": stop
