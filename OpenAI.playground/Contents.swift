e15aaea43249ce465b1b955daf29a10cb90099a6
Zoe Cutler <zcutlerf@gmail.com>
2022-05-04 17:08:41 -0400

    Updated playground to match changes to Engine.

--- OpenAI.playground/Contents.swift
+++ OpenAI.playground/Contents.swift
@@ -14,22 +14,22 @@ let client = OpenAI.Client(apiKey: apiKey)
 
 // MARK: -
 
-let prompt = "Tell me a story.\n"
+let prompt = "Write a tagline for an ice cream shop."
 
-client.completions(engine: "text-curie-001",
+client.completions(engine: "text-davinci-002",
                    prompt: prompt,
                    sampling: .temperature(0.7),
                    numberOfTokens: ...64,
-//                   numberOfCompletions: 5,
-                   echo: true,
-                   stop: [".", "\n", "<|endoftext|>"],
+                   numberOfCompletions: 1,
+                   echo: false,
+                   stop: ["<|endoftext|>"],
                    presencePenalty: 0,
                    frequencyPenalty: 0,
                    bestOf: 1) { result in
     guard case .success(let completions) = result else { fatalError() }
 
     for choice in completions.flatMap(\.choices) {
-        let sentence = "\(choice.text)."
+        let sentence = "\(choice.text)"
         print(sentence)
     }
 }
